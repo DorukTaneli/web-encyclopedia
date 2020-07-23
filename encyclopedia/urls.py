c57e0e619a231fd3da98bec7ccf8e36158710b02
dtaneli16 <dtaneli16@ku.edu.tr>
2020-07-23 18:18:16 +0300

    add edit page functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
     path("add", views.add, name="add"),
-    path("<str:name>", views.wiki, name="wiki")
+    path("<str:name>", views.wiki, name="wiki"),
+    path("edit", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 
 from . import util
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,6 +15,12 @@ def index(request):
     })
 
 def wiki(request, name):
+    if request.method == "POST":
+        return render(request, "encyclopedia/edit.html", {
+            "name": name,
+            "form": NewWikiForm(initial={"name":name, "content":util.get_entry(name)})
+        })
+
     return render(request, "encyclopedia/wiki.html", {
         "name": name,
         "content": util.get_entry(name)
@@ -36,9 +45,37 @@ def add(request):
             "form": NewWikiForm()
         })
 
+def edit(request):
+    if request.method == "POST":
+        logger.info("POST")
+        form = NewWikiForm(request.POST)
+        if form.is_valid():
+            logger.info("form is valid")
+            name = form.cleaned_data["name"]
+            content = form.cleaned_data["content"]
+            file = open(f"entries/{name}.md", "w")
+            file.write(content)
+            file.close()
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            logger.error("form is not valid")
+            logger.error(form.errors)
+            return render(request, "encyclopedia/edit.html", {
+                "name": name,
+                "form": form
+            })
+    else:
+        logger.info("else")
+        return render(request, "encyclopedia/edit.html", {
+            "name": name,
+            "form": NewWikiForm(initial={"name":name, "content":util.get_entry(name)})
+        })
+
+
 class NewWikiForm(forms.Form):
     name = forms.CharField(label="name")
     content = forms.CharField(widget=forms.Textarea, label="content")
+
 
 def search(request): 
     if request.method == "POST":
